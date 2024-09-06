cdacff34977bc495f5d27b078b47e679707a0961
karmesh sankalp meritia <karmesh9797@gmail.com>
2024-09-06 16:49:16 +0530

    Time: 100 ms (72.17%) | Memory: 17.3 MB (85.40%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.py
+++ 73-set-matrix-zeroes/set-matrix-zeroes.py
@@ -1,14 +1,15 @@
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
+        INF = float('inf')
         def markRow(matrix, i):
             for j in range(len(matrix[0])):
-                if matrix[i][j] != 0 and matrix[i][j] != -675:
-                    matrix[i][j] = -675
+                if matrix[i][j] != 0 and matrix[i][j] != INF:
+                    matrix[i][j] = INF
         
         def markCol(matrix, j):
             for i in range(len(matrix)):
-                if matrix[i][j] != 0 and matrix[i][j] != -675:
-                    matrix[i][j] = -675
+                if matrix[i][j] != 0 and matrix[i][j] != INF:
+                    matrix[i][j] = INF
 
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
@@ -18,5 +19,5 @@ class Solution:
         
         for i in range(len(matrix)):
             for j in range(len(matrix[0])):
-                if matrix[i][j] == -675:
+                if matrix[i][j] == INF:
                     matrix[i][j] = 0
